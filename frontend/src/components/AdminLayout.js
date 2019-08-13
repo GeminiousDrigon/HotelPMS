6dc92aa621804288e25cc7edcebdc531f46ea007
Dominic Vega <davega12.dv@gmail.com>
2019-08-14 01:22:28 +0800

    ye

--- frontend/src/components/AdminLayout.js
+++ frontend/src/components/AdminLayout.js
@@ -5,17 +5,6 @@ import Footer from "./Footer";
 
 export default class AdminLayout extends Component {
     render() {
-        return (
-            <div>
-                <Navbar />
-                <div>
-                    <div style={{ float: 'left'}}>
-                        <Sidebar />
-                    </div>
-                    <div style={{ float: 'right'}}>{this.props.children}</div>
-                </div>
-                <Footer />
-            </div>
-        );
+        return <Sidebar />;
     }
 }
