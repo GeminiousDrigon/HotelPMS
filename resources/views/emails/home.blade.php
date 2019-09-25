e5959653735d628addc59cb29f93960eb1b6ff2f
Dominic Vega <davega12.dv@gmail.com>
2019-09-26 05:26:40 +0800

    a

--- resources/views/emails/home.blade.php
+++ resources/views/emails/home.blade.php
@@ -1,5 +1,29 @@
-<div>Hello <strong>Test</strong>,
-    <p>
 
-        Test</p>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+
+<div style="font-family: Arial, Helvetica, sans-serif;"><center>
+    <div style="margin-top: 2%">
+      <img src="img/logo1.jpg" alt="bluepoolgardenlogo" width="25%">
+    </div>
+    <div class="w3-container">
+        <div class="w3-panel w3-card" style="width: 50%;">
+
+            <p style="font-size: 25px; color: blue;">Hi Junrex, <br>
+                  Thank you for booking at Bluepool Garden!</p>
+
+            <h3>Get your QR Code</h3>
+            <img src="smiley.gif" alt="qrcode" height="200" width="200">
+            <br><br>
+            <p>Please show this Qr code to the frontdesk for check-in</p>
+            
+
+            <img src="img/map.jpg" alt="bluepoolgardenlogo" width="100%">
+            <p>How to get to the Hotel by: Google Map <br> Blue Pool Garden, Tagbilaran North Road, Maribojoc, Bohol</p>
+            <br>
+            <p style="margin-bottom: 5%">Thank you for choosing Bluepool Garden.<br>
+            For further inquiries you may call 09361180320 <br>or send an email at bluepoolgarden2@gmail.com<br></p>
+        </div>
+    </div>
 </div>
